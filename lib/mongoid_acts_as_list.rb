db30324ee008fb290ce7d17d8e145029e1972f3d
Tyralion <piliaiev@gmail.com>
2016-08-20 13:53:04 +0500

    Исправление ошибок

--- lib/mongoid_acts_as_list.rb
+++ lib/mongoid_acts_as_list.rb
@@ -2,28 +2,28 @@
 require 'mongoid_acts_as_list/list'
 
 module MongoidActsAsList
-  
+
   module Base
 
     def acts_as_list(options = {})
 
       include ::MongoidActsAsList::List::InstanceMethods
-      
+
       position_field  = options.fetch(:field, :position).try(:to_sym)
       scope_field     = options.fetch(:scope, nil).try(:to_sym)
 
       field position_field,   type: Integer
-      
+
       # For mongoid 2.4
       unless (::Mongoid::VERSION =~ /\A2.4/).nil?
 
         if scope_field.nil?
-          
+
           default_scope order_by([position_field, :asc])
           index position_field
 
         else
-          
+
           default_scope order_by([position_field, :asc], [scope_field, :asc])
 
           index(
@@ -33,45 +33,49 @@ module MongoidActsAsList
               [ scope_field,    Mongo::ASCENDING ]
             ],
             name: "acts_as_list_indx"
-            
+
           )
 
         end # if
 
       else
-      
+
         # For mongoid 3
 
         if scope_field.nil?
-          
-          default_scope order_by([position_field, :asc])
+
+          default_scope ->() {
+            order_by([position_field, :asc])
+          }
           index({ "#{position_field}".to_sym => 1 })
 
         else
-          
-          default_scope order_by([position_field, :asc], [scope_field, :asc])
+
+          default_scope ->() {
+            order_by([position_field, :asc], [scope_field, :asc])
+          }
 
           index({
 
             "#{position_field}".to_sym  => 1,
             "#{scope_field}".to_sym     => 1
 
-          }, {  
-            name: "acts_as_list_indx"            
+          }, {
+            name: "acts_as_list_indx"
           })
 
         end # if
 
-      end  
+      end
 
       define_method(:acts_as_list_position_field) { position_field }
 
-      define_method(:acts_as_list_scope_fields) { 
+      define_method(:acts_as_list_scope_fields) {
         scope_field.nil? ? nil : { scope_field => self[scope_field] }
       }
 
-      set_callback :create,  :before,  :add_to_list_bottom
-      set_callback :destroy, :before,  :remove_from_list
+      before_create   :add_to_list_bottom
+      before_destroy  :remove_from_list
 
     end # act_as_files
 
--- lib/mongoid_acts_as_list/version.rb
+++ lib/mongoid_acts_as_list/version.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 module MongoidActsAsList
 
-  VERSION = '0.2.1'
+  VERSION = '0.2.2'.freeze
 
 end # MongoidActsAsList
